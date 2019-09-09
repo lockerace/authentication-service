b04b85f76368713e56334b3af9b2d38f89146597
David Meir-Levy <davidmeirlevy@gmail.com>
2019-09-09 21:06:02 +0300

    login with roles

--- server/passport/local-login.js
+++ server/passport/local-login.js
@@ -1,46 +1,51 @@
-const User = require('mongoose').model('User');
-const PassportLocalStrategy = require('passport-local').Strategy;
+const User = require('mongoose').model('User')
+const PassportLocalStrategy = require('passport-local').Strategy
 
 module.exports = new PassportLocalStrategy({
-	usernameField: 'email',
-	passwordField: 'password',
-	session: false,
-	passReqToCallback: true
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false,
+  passReqToCallback: true
 }, (req, email, password, done) => {
-	return User.findOne({email: email.trim()}, (err, user) => {
-		if (err) {
-			return done({code: 'FORM_SUBMISSION_FAILED', info: err});
-		}
+  const query = { email: email.trim() }
+  if (req.body.roles && req.body.roles instanceof Array) {
+    query.roles = { $in: req.body.roles }
+  }
+  return User.findOne(query, (err, user) => {
+    if (err) {
+      return done({ code: 'FORM_SUBMISSION_FAILED', info: err })
+    }
 
-		if (!user) {
-			return done({code: 'INCORRECT_CREDENTIALS'});
-		}
+    if (!user) {
+      return done({ code: 'INCORRECT_CREDENTIALS' })
+    }
 
-		return user.comparePassword(password.trim(), (passwordErr, isMatch) => {
-			if (passwordErr) {
-				return done({code: 'FORM_SUBMISSION_FAILED', info: passwordErr});
-			}
+    return user.comparePassword(password.trim(), (passwordErr, isMatch) => {
+      if (passwordErr) {
+        return done({ code: 'FORM_SUBMISSION_FAILED', info: passwordErr })
+      }
 
-			if (!isMatch) {
-				return done({code: 'INCORRECT_CREDENTIALS'});
-			}
+      if (!isMatch) {
+        return done({ code: 'INCORRECT_CREDENTIALS' })
+      }
 
-			const token = user.getToken();
-			const refreshToken = user.getRefreshToken();
+      const token = user.getToken()
+      const refreshToken = user.getRefreshToken()
 
-			return user.save().then(() => {
-				return done(
-					null,
-					{
-						token,
-						refreshToken,
-						user: {
-							email: user.email,
-							name: user.name,
-						}
-					}
-				);
-			});
-		});
-	});
-});
+      return user.save().then(() => {
+        return done(
+          null,
+          {
+            token,
+            refreshToken,
+            user: {
+              email: user.email,
+              name: user.name,
+              roles: user.roles
+            }
+          }
+        )
+      })
+    })
+  })
+})
