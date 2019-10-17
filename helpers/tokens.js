bbca1feee4756da6f39b3542e4b6e818a9f9d860
David Meir-Levy <davidmeirlevy@gmail.com>
2019-10-17 09:18:24 +0300

    fixed tokens check minor issue

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -3,45 +3,48 @@ const User = require('mongoose').model('User')
 const config = require('../config')
 
 function verifyToken (token) {
-	return verify(token, config.jwtSecret).then(({ user, decoded }) => {
-		if (user.tokenCreated.toJSON() === decoded.created) {
-			return user
-		}
-		return Promise.reject()
-	})
+  if (!token.trim()) {
+    return Promise.reject()
+  }
+  return verify(token, config.jwtSecret).then(({ user, decoded }) => {
+    if (user.tokenCreated.toJSON() === decoded.created) {
+      return user
+    }
+    return Promise.reject()
+  })
 }
 
 function verifyRefreshToken (refreshToken) {
-	return verify(refreshToken, config.refreshTokenSecret).then(({ user, decoded }) => {
-		if (user.refreshTokenCreated.toJSON() === decoded.created) {
-			return user
-		}
-		return Promise.reject()
-	})
+  return verify(refreshToken, config.refreshTokenSecret).then(({ user, decoded }) => {
+    if (user.refreshTokenCreated.toJSON() === decoded.created) {
+      return user
+    }
+    return Promise.reject()
+  })
 }
 
 function verify (token, secret) {
-	return new Promise((resolve, reject) => {
-		jwt.verify(token, secret, (err, decoded) => {
-			if (err || !decoded) {
-				// the 401 code is for unauthorized status
-				return reject(err || {message: 'token is empty'})
-			}
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err || !decoded) {
+        // the 401 code is for unauthorized status
+        return reject(err || { message: 'token is empty' })
+      }
 
-			const userId = decoded.sub
+      const userId = decoded.sub
 
-			// check if a user exists
-			return User.findById(userId, (userErr, user) => {
-				if (userErr || !user) {
-					return reject(userErr || { message: 'user not exists' })
-				}
-				resolve({ user, decoded })
-			})
-		})
-	})
+      // check if a user exists
+      return User.findById(userId, (userErr, user) => {
+        if (userErr || !user) {
+          return reject(userErr || { message: 'user not exists' })
+        }
+        resolve({ user, decoded })
+      })
+    })
+  })
 }
 
 module.exports = {
-	verifyToken,
-	verifyRefreshToken
+  verifyToken,
+  verifyRefreshToken
 }
