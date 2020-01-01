10c38cf238639288dae833772e9b0a571097434d
David Meir-Levy <davidmeirlevy@gmail.com>
2020-01-01 16:50:07 +0200

    auth check valida test

--- server/middleware/tests/auth-check.spec.js
+++ server/middleware/tests/auth-check.spec.js
@@ -1,21 +1,45 @@
 const { onlyAuthenticated } = require('../auth-check')
+const getExpressResMock = require('../../../mocks/express-res.mock')
 
 describe('Auth Check middlewares', () => {
   describe('onlyAuthenticated', () => {
     describe('When get request without a valid user', () => {
+      const emptyRequest = {}
       it('should response error to user', () => {
-        const resMock = jest.fn()
-        const nextMock = jest.fn()
+        const resMock = getExpressResMock()
 
-        resMock.status = jest.fn(() => resMock)
-        resMock.jsonp = jest.fn(() => resMock)
-        resMock.end = jest.fn(() => resMock)
-
-        onlyAuthenticated({}, resMock, nextMock)
+        onlyAuthenticated(emptyRequest, resMock)
 
         expect(resMock.status).toBeCalledWith(401)
         expect(resMock.jsonp).toBeCalledWith({ message: 'you are not authorized' })
         expect(resMock.end).toBeCalled()
+
+      })
+
+      it('should not trigger next middleware', () => {
+        const resMock = getExpressResMock()
+        const nextMock = jest.fn()
+        onlyAuthenticated(emptyRequest, resMock, nextMock)
+
+        expect(nextMock).not.toBeCalled()
+      })
+    })
+
+    describe('When get request with a valid user', () => {
+      const reqWithUser = {
+        user: {}
+      }
+      it('should continue to next middleware', () => {
+        const resMock = getExpressResMock()
+        const nextMock = jest.fn()
+
+        onlyAuthenticated(reqWithUser, resMock, nextMock)
+
+        expect(nextMock).toBeCalled();
+        expect(nextMock.mock.calls.length).toBe(1)
+
+        // next function should be called with not arguments
+        expect(nextMock.mock.calls[0][0]).toBeUndefined()
       })
     })
   })
